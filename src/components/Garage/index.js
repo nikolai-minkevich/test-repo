90eee20cf65b70d5f0619debd72b5c446ffd5c72
Nikolai Minkevich <minkevich.nikolai@yandex.ru>
2021-09-24 14:36:35 +0300

    add something

---
+++ src/components/Garage/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Garage = () => {
+  return (
+    <>
+        <h1>HELLO FROM GARAGE!!!</h1>
+    </>
+  );
+};
+
+export default Garage;
---
+++ src/components/Home/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+        <h1>HELLO FROM HOME!!!</h1>
+    </>
+  );
+};
+
+export default Home;
